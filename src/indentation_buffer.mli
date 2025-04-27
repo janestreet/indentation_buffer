587f164416fd8fc38a6367d4c46d7278f42ee4c5
public-release <as-public-release@janestreet.com>
2025-04-27 21:24:24 +0100

    v0.18~preview.130.26+1192

--- src/indentation_buffer.mli
+++ src/indentation_buffer.mli
@@ -1,18 +1,17 @@
 open! Core
 
-(** A buffer that keeps track of the current indentation level. It abstracts
-    the need for formatting code to thread the current indentation everywhere,
-    and also ensures that blank lines do not accidentally get filled with the
-    current indentation, thus yielding trailing whitespace in the output. *)
+(** A buffer that keeps track of the current indentation level. It abstracts the need for
+    formatting code to thread the current indentation everywhere, and also ensures that
+    blank lines do not accidentally get filled with the current indentation, thus yielding
+    trailing whitespace in the output. *)
 type t
 
-(** Appends the input string to a buffer. If the buffer is at the start of a
-    new line, then the current indentation is inserted. *)
+(** Appends the input string to a buffer. If the buffer is at the start of a new line,
+    then the current indentation is inserted. *)
 val string : t -> string -> unit
 
-(** Starts a new line in the buffer, but does not append any indentation. This
-    will only happen if a string is appended before the buffer moves to the
-    next line. *)
+(** Starts a new line in the buffer, but does not append any indentation. This will only
+    happen if a string is appended before the buffer moves to the next line. *)
 val newline : t -> unit
 
 (** Increase the current indentation by one level. *)
